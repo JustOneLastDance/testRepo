fdf973136c45c51b5a717bb8d05d2e944bb89f94
JustinChou <justinchou186@gmail.com>
2017-07-31 11:03:54 +0800

    init the object

---
+++ GitTest/GitTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by JustinChou on 31/07/2017.
+//  Copyright © 2017 JustinChou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
